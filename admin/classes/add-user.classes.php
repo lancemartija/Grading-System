94a601b291b02cf0439ce04af727c93e9fecdd73
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 00:01:24 +0800

    add: add user class

---
+++ admin/classes/add-user.classes.php
@@ -0,0 +1,36 @@
+<?php
+
+class AddUser extends Dbh
+{
+  protected function setUser($name, $email, $uid, $pwd, $type)
+  {
+    $stmt = $this->connect()->prepare('INSERT INTO tbluseraccounts (account_name, account_email, account_username, account_password, account_type) VALUES(?, ?, ?, ?, ?);');
+
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    if (!$stmt->execute([$name, $email, $uid, $hashedPwd, $type])) {
+      $stmt = null;
+      header("Location: ../view/users.php?error=stmtfailed");
+      exit;
+    }
+
+    $stmt = null;
+  }
+
+  protected function checkUser($uid, $email)
+  {
+    $stmt = $this->connect()->prepare('SELECT * FROM tbluseraccounts WHERE account_username = ? OR account_email = ?');
+
+    if (!$stmt->execute([$uid, $email])) {
+      $stmt = null;
+      header('Location: ../view/users.php?error=stmtfailed');
+      exit;
+    }
+
+    if ($stmt->rowCount() > 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
