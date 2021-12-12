bad38daba483a0fe18cd021383b4007704177c12
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 00:07:08 +0800

    update: new placeholder database

--- dbstudentgrading.sql
+++ dbstudentgrading.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2021 at 03:36 PM
+-- Generation Time: Dec 12, 2021 at 05:06 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -31,6 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `tbluseraccounts` (
   `account_id` int(11) NOT NULL,
   `account_name` varchar(255) NOT NULL,
+  `account_email` varchar(255) NOT NULL,
   `account_username` varchar(255) NOT NULL,
   `account_password` text NOT NULL,
   `account_type` varchar(255) NOT NULL
@@ -40,8 +41,9 @@ CREATE TABLE `tbluseraccounts` (
 -- Dumping data for table `tbluseraccounts`
 --
 
-INSERT INTO `tbluseraccounts` (`account_id`, `account_name`, `account_username`, `account_password`, `account_type`) VALUES
-(1, 'test', 'test', '$2y$10$a4335R5fbBuanNsD7G59EeAp.4OJ5y2lFvX.aHBYKg3TxysJn0Npy', 'test');
+INSERT INTO `tbluseraccounts` (`account_id`, `account_name`, `account_email`, `account_username`, `account_password`, `account_type`) VALUES
+(1, 'test', 'test@test.com', 'test', '$2y$10$a4335R5fbBuanNsD7G59EeAp.4OJ5y2lFvX.aHBYKg3TxysJn0Npy', 'test'),
+(2, 'john doe', 'johndoe@email.com', 'johndoe', '$2y$10$pwyonzUNXuwDhUCpwh7Z5Ot5F0tvZnUlfCaNPbf7u0d9xVjHyHJBO', 'administrator');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +63,7 @@ ALTER TABLE `tbluseraccounts`
 -- AUTO_INCREMENT for table `tbluseraccounts`
 --
 ALTER TABLE `tbluseraccounts`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
