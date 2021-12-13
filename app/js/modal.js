a04ce265205803eac6fcd6d3c0b42730a044f433
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 18:11:04 +0800

    feature: modal functionality when edditing user info

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,13 +1,41 @@
 const addUserBtn = document.querySelector('[data-add-button]')
-const closeBtn = document.querySelector('[data-close-button]')
+const editUserBtn = document.querySelectorAll('[data-edit-button]')
+const closeBtn = document.querySelectorAll('[data-close-button]')
 const addUserModal = document.querySelector('#add-user-modal')
+const editUserModal = document.querySelector('#edit-user-modal')
+
+const inputFieldFname = document.querySelector('#firstname')
+const inputFieldLname = document.querySelector('#lastname')
+const inputFieldEmail = document.querySelector('#email')
+const inputFieldUsername = document.querySelector('#username')
 
 addUserBtn.addEventListener('click', () => {
   addUserModal.classList.remove('hidden')
   addUserModal.classList.add('flex')
 })
 
-closeBtn.addEventListener('click', () => {
-  addUserModal.classList.add('hidden')
-  addUserModal.classList.remove('flex')
+editUserBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    let fname = btn.getAttribute('data-fname')
+    let lname = btn.getAttribute('data-lname')
+    let email = btn.getAttribute('data-email')
+    let username = btn.getAttribute('data-username')
+
+    inputFieldFname.value = fname
+    inputFieldLname.value = lname
+    inputFieldEmail.value = email
+    inputFieldUsername.value = username
+
+    editUserModal.classList.remove('hidden')
+    editUserModal.classList.add('flex')
+  })
+})
+
+closeBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    addUserModal.classList.add('hidden')
+    addUserModal.classList.remove('flex')
+    editUserModal.classList.add('hidden')
+    editUserModal.classList.remove('flex')
+  })
 })
