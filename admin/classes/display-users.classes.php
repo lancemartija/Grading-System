59620f57cb5fa5392e4734bf14b61e8e6cf3d38b
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 18:47:19 +0800

    chore: cleanup comments

--- admin/classes/display-users.classes.php
+++ admin/classes/display-users.classes.php
@@ -6,36 +6,22 @@ class DisplayUsers extends Dbh
   {
     $sql = 'SELECT * FROM tbluseraccounts';
     $stmt = $this->connect()->query($sql);
+    $result = 0;
 
     if (!$stmt) {
       $stmt = null;
       header('Location: ../view/users.php?error=stmtfailed');
       exit;
-    }
+    } else {
+      while ($row = $stmt->fetchAll(PDO::FETCH_ASSOC)) {
+        $result = $row;
+      }
 
-    while ($row = $stmt->fetchAll(PDO::FETCH_ASSOC)) {
-      $result = $row;
+      $stmt = null;
+      return $result;
     }
 
     $stmt = null;
     return $result;
   }
-
-  // protected function checkUserCount()
-  // {
-  //   $sql = 'SELECT * FROM tbluseraccounts';
-  //   $stmt = $this->connect()->query($sql);
-
-  //   if (!$stmt) {
-  //     $stmt = null;
-  //     header('Location: ../view/users.php?error=stmtfailed');
-  //     exit;
-  //   }
-
-  //   if ($stmt->rowCount() > 0) {
-  //     return false;
-  //   }
-
-  //   return true;
-  // }
 }
