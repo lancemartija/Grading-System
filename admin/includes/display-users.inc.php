576a6d686a29063950ac20159ba12888ab565c9c
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 23:14:30 +0800

    feature: search functionality to display specific users

--- admin/includes/display-users.inc.php
+++ admin/includes/display-users.inc.php
@@ -8,6 +8,15 @@ $display = new DisplayUsersContr;
 $result = $display->fetchUsers();
 
 if (!empty($result)) {
+  if (isset($_GET['search']) && !empty(trim($_GET['search']))) {
+    $query = $_GET['search'];
+    $result = $display->fetchUser($query);
+
+    if (empty($result)) {
+      exit;
+    }
+  }
+
   include_once '../templates/user-table.php';
   include_once '../templates/edit-user-modal.php';
 }
