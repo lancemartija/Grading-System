7251c3c4297e7df813a2c3dd99050f5caebe2d03
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 23:13:06 +0800

    add: controller for fetching specific user

--- admin/controller/display-users.contr.php
+++ admin/controller/display-users.contr.php
@@ -2,9 +2,18 @@
 
 class DisplayUsersContr extends DisplayUsers
 {
+  private $query;
+
   public function fetchUsers()
   {
     $result = $this->getUsers();
+    return $result;
+  }
+
+  public function fetchUser($query)
+  {
+    $this->query = $query;
+    $result = $this->getUser($this->query);
     return $result;
   }
 }
