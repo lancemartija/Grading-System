fcff2c72822d433bede15f47338e3bd7617db18d
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 18:09:37 +0800

    add: includes file for fetching user data

---
+++ admin/includes/display-users.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once '../classes/database.classes.php';
+include_once '../classes/display-users.classes.php';
+include_once '../controller/display-users.contr.php';
+
+$display = new DisplayUsersContr;
+$result = $display->fetchUsers();
+
+include_once '../templates/user-table.php';
+include_once '../templates/edit-user-modal.php';
