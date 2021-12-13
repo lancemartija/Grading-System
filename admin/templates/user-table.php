17735f9b0abf2b3a4410c5d65a555ab39a392cb6
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 20:32:54 +0800

    add: data-id for form submit

--- admin/templates/user-table.php
+++ admin/templates/user-table.php
@@ -1,72 +1,74 @@
-<div class="flex flex-col">
-  <div class="overflow-x-auto">
-    <div class="inline-block min-w-full align-middle">
-      <div class="overflow-hidden shadow">
-        <table class="min-w-full divide-y divide-gray-200 table-fixed">
-          <thead class="bg-gray-100">
-            <tr>
-              <th scope="col" class="p-4">
-                <div class="flex items-center">
-                  <input id="checkbox-all" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-cyan-200">
-                  <label for="checkbox-all" class="sr-only">checkbox</label>
-                </div>
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase">
-                Name
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase">
-                Position
-              </th>
-              <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200 ">
-            <?php foreach ($result as $data) : ?>
-              <tr class="hover:bg-gray-100 group">
-                <td class="w-4 p-4">
-                  <div class="flex items-center">
-                    <input id="checkbox-1" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-cyan-200">
-                    <label for="checkbox-1" class="sr-only">checkbox</label>
-                  </div>
-                </td>
-                <td class="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap lg:mr-0">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-gray-500 group-hover:text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                  <div class="text-sm font-normal text-gray-500">
-                    <div class="text-base font-semibold text-gray-900 capitalize"><?= $data['account_firstname'] . ' ' . $data['account_lastname']; ?></div>
-                    <div class="text-sm font-normal text-gray-500"><?= $data['account_email']; ?></div>
-                  </div>
-                </td>
-                <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap">
-                  <div class="flex items-center capitalize">
-                    <?= $data['account_type']; ?>
-                  </div>
-                </td>
-                <td class="p-4 space-x-2 whitespace-nowrap">
-                  <button type="button" data-edit-button data-fname="<?= $data['account_firstname']; ?>" data-lname="<?= $data['account_lastname']; ?>" data-email="<?= $data['account_email']; ?>" data-username="<?= $data['account_username']; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200">
-                    <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
-                      <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
-                    </svg>
-                    Edit user
-                  </button>
-                  <button type="button" data-delete-button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300">
-                    <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                    </svg>
-                    Delete user
-                  </button>
-                </td>
-              </tr>
-            <?php endforeach; ?>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+<div class="flex flex-col min-w-full overflow-hidden overflow-x-auto align-middle shadow">
+  <table class="min-w-full divide-y divide-gray-200 table-fixed">
+    <thead class="bg-gray-100">
+      <tr>
+        <th scope="col" class="p-4">
+          <div class="flex items-center">
+            <input id="checkbox" onclick="toggleAllCheckbox(this)" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-cyan-200">
+            <label for="checkbox" class="sr-only">checkbox</label>
+          </div>
+        </th>
+        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase">
+          ID
+        </th>
+        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase">
+          Name
+        </th>
+        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase">
+          Position
+        </th>
+        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase">
+          Actions
+        </th>
+      </tr>
+    </thead>
+    <tbody class="bg-white divide-y divide-gray-200 ">
+      <?php foreach ($result as $data) : ?>
+        <tr class="hover:bg-gray-100 group">
+          <td class="w-4 p-4">
+            <div class="flex items-center">
+              <input id="checkbox" aria-describedby="checkbox" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-cyan-200">
+              <label for="checkbox" class="sr-only">checkbox</label>
+            </div>
+          </td>
+          <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap">
+            <div class="flex items-center">
+              <?= $data['account_id']; ?>
+            </div>
+          </td>
+          <td class="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap lg:mr-0">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-gray-500 group-hover:text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <div class="text-sm font-normal text-gray-500">
+              <div class="text-base font-semibold text-gray-900 capitalize"><?= $data['account_firstname'] . ' ' . $data['account_lastname']; ?></div>
+              <div class="text-sm font-normal text-gray-500"><?= $data['account_email']; ?></div>
+            </div>
+          </td>
+          <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap">
+            <div class="flex items-center capitalize">
+              <?= $data['account_type']; ?>
+            </div>
+          </td>
+          <td class="p-4 space-x-2 whitespace-nowrap">
+            <button type="button" data-edit-button data-id="<?= $data['account_id']; ?>" data-fname="<?= $data['account_firstname']; ?>" data-lname="<?= $data['account_lastname']; ?>" data-email="<?= $data['account_email']; ?>" data-username="<?= $data['account_username']; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200">
+              <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
+              </svg>
+              Edit user
+            </button>
+            <button type="button" data-delete-button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300">
+              <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+              </svg>
+              Delete user
+            </button>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 </div>
 <!-- <div class="sticky bottom-0 right-0 items-center w-full p-4 bg-white border-t border-gray-200 sm:flex sm:justify-between">
   <div class="flex items-center mb-4 sm:mb-0">
