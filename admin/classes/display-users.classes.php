1f79a3dd6ddabd37f710d68fdacf61bf86051ea0
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 23:13:47 +0800

    add: functionality to fetch specific users

--- admin/classes/display-users.classes.php
+++ admin/classes/display-users.classes.php
@@ -24,4 +24,31 @@ class DisplayUsers extends Dbh
     $stmt = null;
     return $result;
   }
+
+  protected function getUser($query)
+  {
+    $stmt = $this->connect()->prepare('SELECT * FROM tbluseraccounts WHERE account_id = ? OR account_firstname = ? OR account_lastname = ? OR account_email = ? OR account_username = ? OR account_type = ?');
+    $result = 0;
+
+    if (!$stmt->execute([$query, $query, $query, $query, $query, $query])) {
+      $stmt = null;
+      header('Location: ../view/users.php?error=stmtfailed');
+      exit;
+    } else {
+      if ($stmt->rowCount() == 0) {
+        $stmt = null;
+        return $result;
+      } else {
+        while ($row = $stmt->fetchAll(PDO::FETCH_ASSOC)) {
+          $result = $row;
+        }
+      }
+
+      $stmt = null;
+      return $result;
+    }
+
+    $stmt = null;
+    return $result;
+  }
 }
