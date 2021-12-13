6771e6d4aa09dfe2fd0cd27fce220550a0ee2821
Lance Martija <jacobmartija03@gmail.com>
2021-12-13 18:47:50 +0800

    chore: cleanup comments

--- admin/controller/display-users.contr.php
+++ admin/controller/display-users.contr.php
@@ -4,13 +4,7 @@ class DisplayUsersContr extends DisplayUsers
 {
   public function fetchUsers()
   {
-    // if ($this->checkUserCount() == false) {
-    //   header("Location: ../view/users.php?error=userempty");
-    //   exit;
-    // }
-
     $result = $this->getUsers();
-
     return $result;
   }
 }
