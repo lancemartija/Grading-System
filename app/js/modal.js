4c2d9427e9b7fa2305ec560f0639227a87d997dd
Lance Martija <jacobmartija03@gmail.com>
2021-12-14 11:19:33 +0800

    feature: toggle modal functionality for delete user

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,9 +1,11 @@
 const addUserBtn = document.querySelector('[data-add-button]')
 const editUserBtn = document.querySelectorAll('[data-edit-button]')
+const deleteUserBtn = document.querySelectorAll('[data-delete-button]')
 const closeBtn = document.querySelectorAll('[data-close-button]')
 
 const addUserModal = document.querySelector('#add-user-modal')
 const editUserModal = document.querySelector('#edit-user-modal')
+const deleteUserModal = document.querySelector('#delete-user-modal')
 
 const inputFieldId = document.querySelector('#id')
 const inputFieldFname = document.querySelector('#firstname')
@@ -35,11 +37,24 @@ editUserBtn.forEach((btn) => {
   })
 })
 
+deleteUserBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    let id = btn.getAttribute('data-id')
+
+    inputFieldId.value = id
+
+    deleteUserModal.classList.remove('hidden')
+    deleteUserModal.classList.add('flex')
+  })
+})
+
 closeBtn.forEach((btn) => {
   btn.addEventListener('click', () => {
     addUserModal.classList.add('hidden')
     addUserModal.classList.remove('flex')
     editUserModal.classList.add('hidden')
     editUserModal.classList.remove('flex')
+    deleteUserModal.classList.add('hidden')
+    deleteUserModal.classList.remove('flex')
   })
 })
