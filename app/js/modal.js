04c5308f37cc1a383df63a3b9e29950d25b384eb
Lance Martija <jacobmartija03@gmail.com>
2021-12-14 12:31:45 +0800

    fix: setting value for account type

--- app/js/modal.js
+++ app/js/modal.js
@@ -7,11 +7,12 @@ const addUserModal = document.querySelector('#add-user-modal')
 const editUserModal = document.querySelector('#edit-user-modal')
 const deleteUserModal = document.querySelector('#delete-user-modal')
 
-const inputFieldId = document.querySelector('#id')
+const inputFieldId = document.querySelectorAll('#id')
 const inputFieldFname = document.querySelector('#firstname')
 const inputFieldLname = document.querySelector('#lastname')
 const inputFieldEmail = document.querySelector('#email')
 const inputFieldUsername = document.querySelector('#username')
+const inputFieldAccountType = document.querySelector('#accounttype')
 
 addUserBtn.addEventListener('click', () => {
   addUserModal.classList.remove('hidden')
@@ -25,12 +26,14 @@ editUserBtn.forEach((btn) => {
     let lname = btn.getAttribute('data-lname')
     let email = btn.getAttribute('data-email')
     let username = btn.getAttribute('data-username')
+    let accountType = btn.getAttribute('data-type')
 
-    inputFieldId.value = id
+    inputFieldId[0].value = id
     inputFieldFname.value = fname
     inputFieldLname.value = lname
     inputFieldEmail.value = email
     inputFieldUsername.value = username
+    inputFieldAccountType.value = accountType
 
     editUserModal.classList.remove('hidden')
     editUserModal.classList.add('flex')
@@ -41,7 +44,7 @@ deleteUserBtn.forEach((btn) => {
   btn.addEventListener('click', () => {
     let id = btn.getAttribute('data-id')
 
-    inputFieldId.value = id
+    inputFieldId[1].value = id
 
     deleteUserModal.classList.remove('hidden')
     deleteUserModal.classList.add('flex')
