20c24826fe1f0fe3662b08abdc96098984d42ba1
Lance Martija <jacobmartija03@gmail.com>
2021-12-14 17:59:42 +0800

    chore: cleanup extra quot

--- admin/templates/modal/view-user-modal.php
+++ admin/templates/modal/view-user-modal.php
@@ -27,9 +27,9 @@
           </div>
           <div class="col-span-6 sm:col-span-3">
             <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
-            <input id="username" type="text" name="username" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-gray-600/50 focus:border-gray-600 focus:outline-none block w-full p-2.5" " readonly>
+            <input id="username" type="text" name="username" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-gray-600/50 focus:border-gray-600 focus:outline-none block w-full p-2.5" readonly>
           </div>
-          <div class="col-span-6  sm:col-span-3">
+          <div class="col-span-6 sm:col-span-3">
             <label for="accounttype" class="block mb-2 text-sm font-medium text-gray-900">Account Type</label>
             <select id="accounttype" name="accounttype" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-gray-600/50 focus:border-gray-600 focus:outline-none block w-full p-2.5" disabled>
               <option value="administrator">Administrator</option>
