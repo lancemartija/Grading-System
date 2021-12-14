e56914aa53636cd2ceec97c81862efc09c757e31
Lance Martija <jacobmartija03@gmail.com>
2021-12-14 14:18:10 +0800

    change: href variable to different var

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -45,12 +45,12 @@
           </button>
         </form>
         <div class="flex pl-0 mt-3 space-x-1 sm:pl-2 sm:mt-0">
-          <a href="<?= $page ?>.php" class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 sm:w-auto">
+          <a href="<?= $link ?>.php" class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 sm:w-auto">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
             </svg>
-            View all
+            <span>View all</span>
           </a>
         </div>
       </div>
@@ -59,7 +59,7 @@
           <svg class="w-6 h-6 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
           </svg>
-          Add <?= $btn; ?>
+          <span>Add <?= $btn; ?></span>
         </button>
       </div>
     </div>
