ab3f539a19ac0149292a2b94fcb0576e157996d4
Lance Martija <jacobmartija03@gmail.com>
2021-12-15 19:37:09 +0800

    chore: change input field focus colors

--- admin/page/faculty/edit-faculty-modal.php
+++ admin/page/faculty/edit-faculty-modal.php
@@ -20,23 +20,23 @@
           <div class="grid grid-cols-6 gap-6">
             <div class="col-span-6 sm:col-span-3">
               <label for="code" class="block mb-2 text-sm font-medium text-gray-900">Code</label>
-              <input id="code" type="number" name="code" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-green-600/50 focus:border-green-600 focus:outline-none block w-full p-2.5" placeholder="0000" required>
+              <input id="code" type="number" name="code" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-cyan-600/50 focus:border-cyan-600 focus:outline-none block w-full p-2.5" placeholder="0000" required>
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label>
-              <input id="name" type="text" name="name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-green-600/50 focus:border-green-600 focus:outline-none block w-full p-2.5" placeholder="First Name, Middle Name, Last Name" required>
+              <input id="name" type="text" name="name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-cyan-600/50 focus:border-cyan-600 focus:outline-none block w-full p-2.5" placeholder="First Name, Middle Name, Last Name" required>
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
-              <input id="email" type="email" name="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-green-600/50 focus:border-green-600 focus:outline-none block w-full p-2.5" placeholder="example@email.com" required>
+              <input id="email" type="email" name="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-cyan-600/50 focus:border-cyan-600 focus:outline-none block w-full p-2.5" placeholder="example@email.com" required>
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label for="address" class="block mb-2 text-sm font-medium text-gray-900">Address</label>
-              <input id="address" type="text" name="address" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-green-600/50 focus:border-green-600 focus:outline-none block w-full p-2.5" placeholder="Address" required>
+              <input id="address" type="text" name="address" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-cyan-600/50 focus:border-cyan-600 focus:outline-none block w-full p-2.5" placeholder="Address" required>
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label for="gender" class="block mb-2 text-sm font-medium text-gray-900">Gender</label>
-              <select id="gender" name="gender" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-green-600/50 focus:border-green-600 focus:outline-none block w-full p-2.5">
+              <select id="gender" name="gender" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-cyan-600/50 focus:border-cyan-600 focus:outline-none block w-full p-2.5">
                 <option value="" disabled selected hidden>Choose Gender...</option>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
@@ -46,7 +46,7 @@
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label for="status" class="block mb-2 text-sm font-medium text-gray-900">Status</label>
-              <select id="status" name="status" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-green-600/50 focus:border-green-600 focus:outline-none block w-full p-2.5">
+              <select id="status" name="status" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-cyan-600/50 focus:border-cyan-600 focus:outline-none block w-full p-2.5">
                 <option value="" disabled selected hidden>Choose Status...</option>
                 <option value="Single">Single</option>
                 <option value="Female">Married</option>
@@ -56,7 +56,7 @@
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label for="empstatus" class="block mb-2 text-sm font-medium text-gray-900">Employment Status</label>
-              <select id="empstatus" name="empstatus" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-green-600/50 focus:border-green-600 focus:outline-none block w-full p-2.5">
+              <select id="empstatus" name="empstatus" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-cyan-600/50 focus:border-cyan-600 focus:outline-none block w-full p-2.5">
                 <option value="" disabled selected hidden>Choose Employment Status...</option>
                 <option value="Full-time">Full-time</option>
                 <option value="Part-time">Part-time</option>
