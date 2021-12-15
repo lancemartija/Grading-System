7abb58e3b354485dab7cf03fe3444e52a8e09784
Lance Martija <jacobmartija03@gmail.com>
2021-12-15 23:56:16 +0800

    udpate: new class table

--- dbgradingsystem.sql
+++ dbgradingsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2021 at 01:10 PM
+-- Generation Time: Dec 15, 2021 at 04:55 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.26
 
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tblclass`
+--
+
+CREATE TABLE `tblclass` (
+  `class_id` int(11) NOT NULL,
+  `class_name` varchar(255) NOT NULL,
+  `class_code` int(11) NOT NULL,
+  `subject_code` int(11) NOT NULL,
+  `inst_code` int(11) NOT NULL,
+  `subject_ay` varchar(255) NOT NULL,
+  `class_time` varchar(255) NOT NULL,
+  `room_name` varchar(255) NOT NULL,
+  `class_section` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tblcourse`
 --
 
@@ -40,7 +58,8 @@ CREATE TABLE `tblcourse` (
 --
 
 INSERT INTO `tblcourse` (`course_id`, `course_code`, `course_name`, `course_level`, `course_desc`) VALUES
-(1, 1235, 'Grade 11', 11, 'Senior High School');
+(1, 1235, 'Grade 11', 11, 'Senior High School'),
+(2, 1525, 'Grade 12', 12, 'Senior High School');
 
 -- --------------------------------------------------------
 
@@ -60,8 +79,9 @@ CREATE TABLE `tbldepartment` (
 --
 
 INSERT INTO `tbldepartment` (`dept_id`, `dept_code`, `dept_name`, `dept_desc`) VALUES
-(1, 1112, 'CITE', 'College Department'),
-(2, 1113, 'Lavoxa Publishing', 'College Department');
+(1, 1112, 'CITE Department', 'College Department'),
+(2, 1113, 'Lavoxa Publishing', 'College Department'),
+(3, 3136, 'CEAS Department', 'College Department');
 
 -- --------------------------------------------------------
 
@@ -85,7 +105,7 @@ CREATE TABLE `tblinstructor` (
 --
 
 INSERT INTO `tblinstructor` (`inst_id`, `inst_code`, `inst_name`, `inst_address`, `inst_gender`, `inst_status`, `inst_email`, `inst_empstatus`) VALUES
-(1, 111, 'Jimmy Nickelson Cricket', 'Texas, USA', 'Male', 'Single', 'jimminy.cricket@email.com', 'Full-time'),
+(1, 111, 'Jimmy Nickelson Cricket', 'Alberta, Canada', 'Male', 'Single', 'jimminy.cricket@email.com', 'Contract'),
 (2, 1152, 'Maya Willis Hawke', 'Manila, PH', 'Female', 'Single', 'hawke_maya17@email.com', 'Intern');
 
 -- --------------------------------------------------------
@@ -107,8 +127,10 @@ CREATE TABLE `tblrooms` (
 
 INSERT INTO `tblrooms` (`room_id`, `room_code`, `room_name`, `room_desc`) VALUES
 (1, 1235, 'Room 404', 'Not Found'),
-(2, 1242, 'Room 401', 'Movie Room'),
-(3, 1253, 'Room 400', 'Science Room');
+(2, 1242, 'Room 402', 'Movie Room'),
+(3, 1253, 'Room 400', 'Science Room'),
+(4, 1265, 'Room 102', 'Principal&#39;s Office'),
+(5, 1263, 'Room 107', 'Section A');
 
 -- --------------------------------------------------------
 
@@ -131,8 +153,10 @@ CREATE TABLE `tblsubjects` (
 --
 
 INSERT INTO `tblsubjects` (`subj_id`, `subj_code`, `subj_name`, `subj_desc`, `subj_unit`, `subj_ay`, `subj_sem`) VALUES
-(1, 1234, 'Eloquent English', 'Eloquent English 101', 3, '2021-2022', 'First Semester'),
-(2, 1235, 'Advanced Math', 'Mathematics Made Easy', 1, '2021-2022', 'First Semester');
+(1, 1265, 'Eloquent English', 'Eloquent English 101', 3, '2021-2022', 'First Semester'),
+(2, 6353, 'Advanced Math', 'Mathematics Made Easy', 3, '2021-2022', 'First Semester'),
+(3, 6213, 'Algebra 101', 'Algebra Made Easy', 3, '2021-2022', 'Second Semster'),
+(4, 3111, 'Hangul 101', 'Foreign Languages', 2, '2022-2023', 'Summer');
 
 -- --------------------------------------------------------
 
@@ -155,14 +179,20 @@ CREATE TABLE `tbluseraccounts` (
 --
 
 INSERT INTO `tbluseraccounts` (`account_id`, `account_firstname`, `account_lastname`, `account_email`, `account_username`, `account_password`, `account_type`) VALUES
-(1, 'lance', 'martija', 'lance_jacob_martija@dlsl.edu.ph', 'lance', '$2y$10$0tJChunvoQfeTjM3qQiwAOKp.aZMGbStJJFpZwVsCmyDiy7mHKX7m', 'Student'),
-(2, 'john', 'doe', 'johndoe@email.com', 'johndoe', '$2y$10$wLfQvh7osizhE2lh3MSCaupBaqrHLgesPNMamHTmkQ5/CZvQLSc6S', 'Administrator'),
-(3, 'jane', 'doe', 'jane.doe@email.com', 'janedoe', '$2y$10$8exZgPMoCHdyXMZx97HcE.Ks.nGKNGvXaLh6oAa1W8qpT38OEMySS', 'Registrar'),
-(4, 'Michael', 'Stallenhaag', 'stallenhaag.michael@email.com', 'Stallen72', '$2y$10$rxUhWXWVUddCzaklKRF.8u2.xx4XrsnYaVmsig5CLQscZcLYQbtaC', 'Administrator');
+(1, 'john', 'doe', 'johndoe@email.com', 'johndoe', '$2y$10$wLfQvh7osizhE2lh3MSCaupBaqrHLgesPNMamHTmkQ5/CZvQLSc6S', 'Administrator'),
+(2, 'jane', 'doe', 'jane.doe@email.com', 'janedoe', '$2y$10$8exZgPMoCHdyXMZx97HcE.Ks.nGKNGvXaLh6oAa1W8qpT38OEMySS', 'Registrar'),
+(3, 'Michael', 'Stallenhaag', 'stallenhaag.michael@email.com', 'Stallen72', '$2y$10$j3N1Jt4khfkjPJOhO239feXuvValyqz0FXb1YUTiuNxsJI9ol2Qzm', 'Course In-charge'),
+(4, 'test', 'test', 'test@email.com', 'test', '$2y$10$L2R1hdWowC5VfE553i2UsO//KWSBJLOHKTz93IgK.90dT7mTBrp/2', 'Administrator');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tblclass`
+--
+ALTER TABLE `tblclass`
+  ADD PRIMARY KEY (`class_id`);
 
 --
 -- Indexes for table `tblcourse`
@@ -205,16 +235,22 @@ ALTER TABLE `tbluseraccounts`
 --
 
 --
+-- AUTO_INCREMENT for table `tblclass`
+--
+ALTER TABLE `tblclass`
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tblcourse`
 --
 ALTER TABLE `tblcourse`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbldepartment`
 --
 ALTER TABLE `tbldepartment`
-  MODIFY `dept_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `dept_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tblinstructor`
@@ -226,13 +262,13 @@ ALTER TABLE `tblinstructor`
 -- AUTO_INCREMENT for table `tblrooms`
 --
 ALTER TABLE `tblrooms`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tblsubjects`
 --
 ALTER TABLE `tblsubjects`
-  MODIFY `subj_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `subj_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbluseraccounts`
