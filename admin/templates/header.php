3a32128f8085d3a3ac691dc321cf103b02a356bf
Lance Martija <jacobmartija03@gmail.com>
2021-12-17 01:40:46 +0800

    change: simplify button classes

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -60,7 +60,7 @@
         </div>
       </div>
       <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
-        <button type="button" data-add-button class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white bg-green-600 rounded-lg <?= (empty($options) && $page == 'List of Loads') ? 'opacity-70' : 'hover:bg-green-700'; ?> focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 sm:w-auto" <?= (empty($options) && $page == 'List of Loads') ? 'disabled' : ''; ?>>
+        <button type="button" data-add-button class="<?= (empty($options) && $page == 'List of Loads') ? 'btn-header-green-disabled' : 'btn-header-green'; ?>" <?= (empty($options) && $page == 'List of Loads') ? 'disabled' : ''; ?>>
           <svg class="w-6 h-6 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
           </svg>
