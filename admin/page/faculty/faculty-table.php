f3505ece58fa17af4651f2361fdbbca3a3dcb0b0
Lance Martija <jacobmartija03@gmail.com>
2021-12-16 19:04:43 +0800

    chore: change focus ring color

--- admin/page/faculty/faculty-table.php
+++ admin/page/faculty/faculty-table.php
@@ -62,7 +62,7 @@
             </div>
           </td>
           <td class="p-4 space-x-2 whitespace-nowrap">
-            <a href="loads.php?code=<?= $data['inst_code']; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:ring-gray-200">
+            <a href="loads.php?code=<?= $data['inst_code']; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-400/50">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
                 <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" />
