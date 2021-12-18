5d5f17a3a223f7f2d0fc4bd5a95271aeae8920cf
Lance Martija <jacobmartija03@gmail.com>
2021-12-18 14:05:57 +0800

    update: add view modal for student records

--- admin/page/records/records-table.php
+++ admin/page/records/records-table.php
@@ -66,7 +66,7 @@
             </a>
           </td>
           <td class="p-4 space-x-2 text-right whitespace-nowrap">
-            <button type="button" data-view-button data-code="<?= $data['course_code']; ?>" data-name="<?= $data['course_name']; ?>" data-level="<?= $data['course_level']; ?>" data-desc="<?= $data['course_desc']; ?>" class="btn-gray">
+            <button type="button" data-view-button data-date="<?= $data['sy_enrolldate']; ?>" data-sem="<?= $data['sy_sem']; ?>" data-status="<?= $data['sy_status']; ?>" data-level="<?= $data['sy_level']; ?>" data-year="<?= $data['sy_ay']; ?>" class="btn-gray">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                 <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
--- src/js/records-modal.js
+++ src/js/records-modal.js
@@ -1,21 +1,44 @@
 const addRecordsBtn = document.querySelector('[data-add-button]')
+const viewRecordsBtn = document.querySelectorAll('[data-view-button]')
 const editRecordsBtn = document.querySelectorAll('[data-edit-button]')
 const deleteRecordsBtn = document.querySelectorAll('[data-delete-button]')
 const closeBtn = document.querySelectorAll('[data-close-button]')
 
 const addRecordsModal = document.querySelector('#add-record-modal')
+const viewRecordsModal = document.querySelector('#view-record-modal')
 const editRecordsModal = document.querySelector('#edit-record-modal')
 const deleteRecordsModal = document.querySelector('#delete-record-modal')
 
 const inputFieldId = document.querySelectorAll('#id')
-const inputFieldSem = document.querySelector('#sem')
-const inputFieldStatus = document.querySelector('#status')
-const inputFieldLevel = document.querySelector('#level')
-const inputFieldYear = document.querySelector('#year')
+const inputFieldSem = document.querySelectorAll('#sem')
+const inputFieldStatus = document.querySelectorAll('#status')
+const inputFieldLevel = document.querySelectorAll('#level')
+const inputFieldYear = document.querySelectorAll('#year')
+const inputFieldDate = document.querySelector('#date')
 
 addRecordsBtn.addEventListener('click', () => {
   addRecordsModal.classList.remove('hidden')
   addRecordsModal.classList.add('flex')
+})
+
+viewRecordsBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    let id = btn.getAttribute('data-id')
+    let sem = btn.getAttribute('data-sem')
+    let status = btn.getAttribute('data-status')
+    let level = btn.getAttribute('data-level')
+    let year = btn.getAttribute('data-year')
+    let date = btn.getAttribute('data-date')
+
+    inputFieldSem[0].value = sem
+    inputFieldStatus[0].value = status
+    inputFieldLevel[0].value = level
+    inputFieldYear[0].value = year
+    inputFieldDate.value = date
+
+    viewRecordsModal.classList.remove('hidden')
+    viewRecordsModal.classList.add('flex')
+  })
 })
 
 editRecordsBtn.forEach((btn) => {
@@ -27,10 +50,10 @@ editRecordsBtn.forEach((btn) => {
     let year = btn.getAttribute('data-year')
 
     inputFieldId[0].value = id
-    inputFieldSem.value = sem
-    inputFieldStatus.value = status
-    inputFieldLevel.value = level
-    inputFieldYear.value = year
+    inputFieldSem[1].value = sem
+    inputFieldStatus[1].value = status
+    inputFieldLevel[1].value = level
+    inputFieldYear[1].value = year
 
     editRecordsModal.classList.remove('hidden')
     editRecordsModal.classList.add('flex')
@@ -52,6 +75,8 @@ closeBtn.forEach((btn) => {
   btn.addEventListener('click', () => {
     addRecordsModal.classList.add('hidden')
     addRecordsModal.classList.remove('flex')
+    viewRecordsModal.classList.add('hidden')
+    viewRecordsModal.classList.remove('flex')
     editRecordsModal.classList.add('hidden')
     editRecordsModal.classList.remove('flex')
     deleteRecordsModal.classList.add('hidden')
