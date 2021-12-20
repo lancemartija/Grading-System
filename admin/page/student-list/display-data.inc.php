71b9f3b21742a30626336e87dba5972143f5dbd9
Lance Martija <jacobmartija03@gmail.com>
2021-12-20 15:33:52 +0800

    add: list of students page

---
+++ admin/page/student-list/display-data.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$result = $display->fetchData();
+
+if (!empty($result)) {
+  if (isset($_GET['search']) && !empty(trim($_GET['search']))) {
+    $query = $_GET['search'];
+    $result = $display->fetchSearchData($query);
+
+    if (empty($result)) {
+      exit;
+    }
+  }
+
+  include_once 'student-list-table.php';
+}
