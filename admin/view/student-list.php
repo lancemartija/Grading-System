aac38ed7d07f0bd095e5937aeb4a897498645ee1
Lance Martija <jacobmartija03@gmail.com>
2021-12-24 00:17:28 +0800

    remove: asyc defer on jqeury script link

--- admin/view/student-list.php
+++ admin/view/student-list.php
@@ -38,7 +38,7 @@ $i = 0;
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
   <script async defer src="../../src/js/student-list-modal.js"></script>
-  <script async defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
