425df43e86aa91e6520365fe572ed9c14b32e23b
Lance Martija <jacobmartija03@gmail.com>
2021-12-23 23:39:00 +0800

    add: extra error handlers

--- admin/page/dashboard/dashboard-table.php
+++ admin/page/dashboard/dashboard-table.php
@@ -5,31 +5,33 @@
     </div>
     <div class="flow-root">
       <ul role="list" class="divide-y divide-gray-200">
-        <?php foreach ($topperformers as $data) : ?>
-          <li class="py-3 sm:py-4">
-            <div class="flex items-center space-x-4">
-              <div class="flex-shrink-0">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                </svg>
+        <?php if (!empty($topperformers)) { ?>
+          <?php foreach ($topperformers as $data) : ?>
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center space-x-4">
+                <div class="flex-shrink-0">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                  </svg>
+                </div>
+                <div class="flex-1 min-w-0">
+                  <p class="text-sm font-medium text-gray-900 truncate">
+                    <?= $data['student_lname'] . ', ' . $data['student_fname'] . ' ' . $data['student_mname']; ?>
+                  </p>
+                  <p class="text-sm text-gray-500 truncate">
+                    <?= $data['student_email']; ?>
+                  </p>
+                </div>
+                <div class="inline-flex items-center text-base font-light text-gray-400">
+                  <?= $data['subj_name']; ?>
+                </div>
+                <div class="inline-flex items-center text-base font-semibold text-gray-900">
+                  <?= $data['grade_avg']; ?>
+                </div>
               </div>
-              <div class="flex-1 min-w-0">
-                <p class="text-sm font-medium text-gray-900 truncate">
-                  <?= $data['student_lname'] . ', ' . $data['student_fname'] . ' ' . $data['student_mname']; ?>
-                </p>
-                <p class="text-sm text-gray-500 truncate">
-                  <?= $data['student_email']; ?>
-                </p>
-              </div>
-              <div class="inline-flex items-center text-base font-light text-gray-400">
-                <?= $data['subj_name']; ?>
-              </div>
-              <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                <?= $data['grade_avg']; ?>
-              </div>
-            </div>
-          </li>
-        <?php endforeach; ?>
+            </li>
+          <?php endforeach; ?>
+        <?php } ?>
       </ul>
     </div>
   </div>
@@ -50,16 +52,18 @@
           </tr>
         </thead>
         <tbody class="divide-y divide-gray-100">
-          <?php foreach ($result as $data) : ?>
-            <?php $topclasses = $class->dashboardFetch($data['class_code']); ?>
-            <?php foreach ($topclasses as $value) : ?>
-              <tr class="text-gray-500">
-                <th class="p-4 px-4 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap"><?= $data['class_name']; ?></th>
-                <td class="p-4 px-4 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap"><?= $value['COUNT(*)']; ?></td>
-                <td class="p-4 px-4 text-xs align-middle border-t-0 whitespace-nowrap">
-              </tr>
+          <?php if (!empty($topperformers)) { ?>
+            <?php foreach ($result as $data) : ?>
+              <?php $topclasses = $class->dashboardFetch($data['class_code']); ?>
+              <?php foreach ($topclasses as $value) : ?>
+                <tr class="text-gray-500">
+                  <th class="p-4 px-4 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap"><?= $data['class_name']; ?></th>
+                  <td class="p-4 px-4 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap"><?= $value['COUNT(*)']; ?></td>
+                  <td class="p-4 px-4 text-xs align-middle border-t-0 whitespace-nowrap">
+                </tr>
+              <?php endforeach; ?>
             <?php endforeach; ?>
-          <?php endforeach; ?>
+          <?php } ?>
         </tbody>
       </table>
     </div>
