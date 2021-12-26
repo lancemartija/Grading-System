80c6251e3095cac3a8ee3b9b3a62d120a5277bcf
Lance Martija <jacobmartija03@gmail.com>
2021-12-26 16:24:41 +0800

    rename: information center to our team

--- admin/templates/sidebar.php
+++ admin/templates/sidebar.php
@@ -89,11 +89,11 @@
           </li>
         </ul>
         <div class="pt-2 space-y-2">
-          <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group">
+          <a href="ourteam.php" class="flex items-center p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
             </svg>
-            <span class="ml-3">Information Center</span>
+            <span class="ml-3">Our Team</span>
           </a>
         </div>
       </div>
