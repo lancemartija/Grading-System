4bfefee1a7aede9527676f267095428b8e638dd7
Lance Martija <jacobmartija03@gmail.com>
2022-01-01 23:04:32 +0800

    add: dashboard cards

--- student/view/dashboard.php
+++ student/view/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['student']) && !isset($_SESSION['studentid'])) {
+if (!isset($_SESSION['name']) && !isset($_SESSION['studentid'])) {
   header('Location: ../../index.php');
   exit;
 }
@@ -23,12 +23,41 @@ $page = 'Dashboard';
 <body class="h-full">
   <?php include_once '../templates/navbar.php'; ?>
   <main>
-    <div class="px-8 py-6 mx-auto max-w-7xl">
-      <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">
-        <div class="border-4 border-gray-200 border-dashed rounded-lg h-96"></div>
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div class="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
+        <a href="#">
+          <div class="relative grid overflow-hidden duration-200 transform bg-green-600 rounded shadow-lg place-items-center hover:-translate-y-2 hover:shadow-2xl h-80">
+            <img class="w-1/2" src="../../src/img/subjects.png" alt="" />
+            <div class="absolute bottom-0 left-0 right-0 px-6 py-4 space-x-3 duration-200">
+              <p class="text-2xl font-bold text-center text-gray-100">Subjects</p>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="relative grid overflow-hidden duration-200 transform bg-blue-600 rounded shadow-lg place-items-center hover:-translate-y-2 hover:shadow-2xl h-80">
+            <img class="w-1/2" src="../../src/img/grades.png" alt="" />
+            <div class="absolute bottom-0 left-0 right-0 px-6 py-4 space-x-3 duration-200">
+              <p class="text-2xl font-bold text-center text-gray-100">Grades</p>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="relative grid overflow-hidden duration-200 transform bg-gray-600 rounded shadow-lg place-items-center hover:-translate-y-2 hover:shadow-2xl h-80">
+            <img class="w-1/2" src="../../src/img/<?= ($_SESSION['student'][0]['student_gender'] == 'Male') ? 'male' : 'female'; ?>.png" alt="" />
+            <div class="absolute bottom-0 left-0 right-0 px-6 py-4 space-x-3 duration-200">
+              <p class="text-2xl font-bold text-center text-gray-100">Student Info</p>
+            </div>
+          </div>
+        </a>
+        <a href="#" class="cursor-default">
+          <div class="relative grid overflow-hidden duration-200 transform bg-red-600 rounded shadow-lg place-items-center opacity-30 h-80">
+            <img class="w-1/2" src="../../src/img/report.png" alt="" />
+            <div class="absolute bottom-0 left-0 right-0 px-6 py-4 space-x-3 duration-200">
+              <p class="text-2xl font-bold text-center text-gray-100">Reports</p>
+            </div>
+          </div>
+        </a>
       </div>
-      <!-- /End replace -->
     </div>
   </main>
 
