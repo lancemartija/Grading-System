9795adf7e10334dba662edf6b1f7590a6ae06561
Lance Martija <jacobmartija03@gmail.com>
2022-01-02 19:35:58 +0800

    change: a tag to div

--- student/page/subjects/subjects-content.php
+++ student/page/subjects/subjects-content.php
@@ -1,7 +1,7 @@
 <section class="w-full">
   <div class="grid grid-cols-2 gap-4">
     <?php foreach ($result as $data) : ?>
-      <a class="relative block p-4 bg-white border border-gray-100 shadow-sm rounded-xl" href="">
+      <div class="relative block p-4 bg-white border border-gray-100 shadow-sm rounded-xl">
         <div class="absolute top-0 right-0 px-4 py-6 text-gray-300">
           <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
@@ -61,7 +61,7 @@
             </span>
           </div>
         </dl>
-      </a>
+      </div>
     <?php endforeach; ?>
   </div>
 </section>
